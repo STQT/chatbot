f49f4f59f5d0eeb1e5c10010c8245b60493b0c2b
Gayrat <gayratbek.sultonov@gmail.com>
2022-01-15 19:52:19 +0500

    change tokens

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ logging.basicConfig(
 )
 
 
-BOT_TOKEN = "2020786505:AAEf_Hqo_sXIF5xET-SCWTvse1tWjvfTSr0"  # please shu turip tursin :)
-MONGO_URL = 'mongodb+srv://gproduct:qwerty2021@cluster0.sjsk4.mongodb.net/chatbot?retryWrites=true&w=majority'  # shu ham :)
+# BOT_TOKEN = "2020786505:AAEf_Hqo_sXIF5xET-SCWTvse1tWjvfTSr0"  # please shu turip tursin :)
+# MONGO_URL = 'mongodb+srv://gproduct:qwerty2021@cluster0.sjsk4.mongodb.net/chatbot?retryWrites=true&w=majority'
 
-# BOT_TOKEN = "5042886538:AAGUs9OQ0Zd9_Nc33HpDX7IkePu3roh4BME"
-# MONGO_URL = 'mongodb+srv://vodiylik:vodiylik@cluster0.b18ay.mongodb.net/davraBot?retryWrites=true&w=majority'
+BOT_TOKEN = "5042886538:AAGUs9OQ0Zd9_Nc33HpDX7IkePu3roh4BME"
+MONGO_URL = 'mongodb+srv://vodiylik:vodiylik@cluster0.b18ay.mongodb.net/davraBot?retryWrites=true&w=majority'
