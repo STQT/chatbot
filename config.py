b8d351700b24865eed1abaa414c904d5202f61a0
Gayrat <gayratbek.sultonov@gmail.com>
2022-02-05 22:57:51 +0500

    adding new Abbos aka ID

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ logging.basicConfig(
     level=logging.WARNING
 )
 
-admin_ids = 256665985, 390736292
+admin_ids = 256665985, 390736292, 531409948
 group_id = -650304675
 channel_urls_dict = ({
     "title": "Мозийга бир назар",
