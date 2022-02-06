97113ba1f7bab842f9ed0f989cde8b878eb8a0bb
Gayrat <gayratbek.sultonov@gmail.com>
2022-02-06 19:35:36 +0500

    add Abbos aka second profile

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ logging.basicConfig(
     level=logging.WARNING
 )
 
-admin_ids = 256665985, 390736292, 531409948
+admin_ids = 256665985, 390736292, 531409948, 556739283
 group_id = -650304675
 channel_urls_dict = ({
     "title": "Мозийга бир назар",
